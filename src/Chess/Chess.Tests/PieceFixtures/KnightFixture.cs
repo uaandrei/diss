0e5dfb672ffbe5b65b078ac19c45125af4a577aa
andrei <ua.andrei@gmail.com>
2015-04-04 22:54:58 +0300

    added knight

---
+++ src/Chess/Chess.Tests/PieceFixtures/KnightFixture.cs
@@ -0,0 +1,46 @@
+﻿using Chess.Pieces;
+using Xunit;
+
+namespace Chess.Tests.PieceFixtures
+{
+    public class KnightFixture
+    {
+        private Knight _sut;
+
+        public KnightFixture()
+        {
+            _sut = new Knight(new Position(3, 3), PieceColor.Black);
+        }
+
+        [Theory]
+        [InlineData(1, 2)]
+        [InlineData(1, 4)]
+        [InlineData(5, 2)]
+        [InlineData(5, 4)]
+        [InlineData(2, 5)]
+        [InlineData(4, 5)]
+        [InlineData(4, 1)]
+        [InlineData(2, 1)]
+        public void Should_MoveInL(int x, int y)
+        {
+            // arrange
+            var LPosition = new Position(x, y);
+            var wrongPosition = new Position(x - 2, y - 2);
+
+            // act
+            var canMoveToLPosition = _sut.CanMove(LPosition);
+            var canMoveToWrongPosition = _sut.CanMove(wrongPosition);
+
+            // assert
+            Assert.True(canMoveToLPosition);
+            Assert.False(canMoveToWrongPosition);
+        }
+
+        [Fact]
+        public void Should_HavePieceTypeKnight()
+        {
+            // assert
+            Assert.Equal(PieceType.Knight, _sut.Type);
+        }
+    }
+}
---
+++ src/Chess/Chess/Pieces/Knight.cs
@@ -0,0 +1,24 @@
+﻿using Chess.Helpers;
+using System;
+
+namespace Chess.Pieces
+{
+    public class Knight : BasePiece
+    {
+        public Knight(Position p, PieceColor color)
+            : base(p, color)
+        {
+        }
+
+        public override PieceType Type { get { return PieceType.Knight; } }
+
+        public override bool CanMove(Position p)
+        {
+            var xDist = Math.Abs(PositionCalculator.GetXDistance(_curPosition, p));
+            var yDist = Math.Abs(PositionCalculator.GetYDistance(_curPosition, p));
+
+            return (xDist == 2 && yDist == 1)
+                || (xDist == 1 && yDist == 2);
+        }
+    }
+}
