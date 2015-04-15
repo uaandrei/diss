7069973308764af77b83ba745cbbaa7facec5438
uaandrei <ua.andrei@gmail.com>
2015-04-15 11:32:55 +0300

    added piece factory

---
+++ src/Chess/Chess/Pieces/PieceFactory.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Chess.Pieces {
+  public class PieceFactory {
+    public IPiece GetPieceForStartingPosition(int x, int y) {
+      throw new NotImplementedException();
+    }
+  }
+}
