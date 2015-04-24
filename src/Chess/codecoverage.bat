4666efa5d0c3c2c9536b40461327888859a721ec
andrei <ua.andrei@gmail.com>
2015-04-24 21:46:37 +0300

    added code coverage using openCover and ReportGenerator

---
+++ src/Chess/codecoverage.bat
@@ -0,0 +1,7 @@
+set opencover_exe="packages\OpenCover.4.5.3723\OpenCover.Console.exe"
+set reportgenerator_exe="packages\ReportGenerator.2.1.4.0\ReportGenerator.exe"
+set test_exe="packages\xunit.runner.console.2.0.0\tools\xunit.console.exe"
+
+%opencover_exe% -output:coverage.xml -target:%test_exe% -targetargs:"%1 -noshadow" -filter:"+[*]* -[xunit*]* -[*Tests]*"
+%reportgenerator_exe% .\coverage.xml .\coverage
+start coverage\index.htm
