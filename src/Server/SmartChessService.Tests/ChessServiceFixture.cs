f3130bc329484fab7a34add6b4cab0307108041f
andrei <ua.andrei@gmail.com>
2015-06-05 01:24:42 +0300

    first commit with working chess service + test

---
+++ src/Server/SmartChessService.Tests/ChessServiceFixture.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+namespace SmartChessService.Tests
+{
+    public class ChessServiceFixture
+    {
+        private ChessService _sut;
+
+        public ChessServiceFixture()
+        {
+            _sut = new ChessService();
+        }
+
+        [Fact]
+        public void GetMoveResponse_ShouldReturnMove_g3g6WithScore_28997()
+        {
+            // arrange
+            var fen = "2rr3k/pp3pp1/1nnqbN1p/3pN3/2pP4/2P3Q1/PPB4P/R4RK1 w - -";
+            var depth = 6;
+            var expectedResponse = "g3g6;28997";
+
+            // act
+            var actualResponse = _sut.GetMoveResponse(fen, depth);
+
+            // assert
+            Assert.Equal(expectedResponse, actualResponse);
+        }
+    }
+}
---
+++ src/Server/SmartChessService/ChessService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SmartChessService
+{
+    public class ChessService
+    {
+        [DllImport("ChessSmartAI.dll", EntryPoint = "GetMove", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr GetMove(string fen, int depth);
+
+        public string GetMoveResponse(string fen, int depth)
+        {
+            var move = GetMove(fen, depth);
+            string result = Marshal.PtrToStringAnsi(move);
+            return result;
+        }
+    }
+}
