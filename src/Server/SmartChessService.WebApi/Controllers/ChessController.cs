11d42c49d05d3c8772b5f437bcc2a61c5fd37f4f
andrei <ua.andrei@gmail.com>
2015-06-05 05:02:11 +0300

    added self hosted WebApi 2.0 web server

---
+++ src/Server/SmartChessService.WebApi/Controllers/ChessController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+
+namespace SmartChessService.WebApi.Controllers
+{
+    public class ChessController : ApiController
+    {
+        public string Get(string fen, int depth)
+        {
+            var service = new ChessService();
+            var moveResponse = service.GetMoveResponse(fen, depth);
+
+            return moveResponse;
+        }
+
+        public string Get()
+        {
+            return "Server State is OK";
+        }
+    }
+}
---
+++ src/Server/SmartChessService.WebApi/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Owin;
+using System.Web.Http;
+
+namespace SmartChessService.WebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "Default",
+                routeTemplate: "{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
