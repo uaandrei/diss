e7cf0986211b7f9b068a4c2b4414e274488a55e7
andrei <ua.andrei@gmail.com>
2015-06-28 22:32:41 +0300

    to be fixed (undone service mate call fix)

--- src/Client/Chess.Business.ImplementationA/GameTable.cs
+++ src/Client/Chess.Business.ImplementationA/GameTable.cs
@@ -186,8 +186,7 @@ namespace Chess.Business.ImplementationA
             var fromPos = new Position(_selectedPiece.CurrentPosition);
             _selectedPiece.Move(userInput);
 
-            var isChess = _rules[RuleNames.Chess].IsSatisfied();
-            if (isChess)
+            if (_rules[RuleNames.Chess].IsSatisfied())
             {
                 UndoLastMove(false);
                 _eventAggregator.GetEvent<Chess.Infrastructure.Events.MessageEvent>().Publish(new MessageInfo(1500, "Move illegal! King in check."));
@@ -201,7 +200,7 @@ namespace Chess.Business.ImplementationA
                 _playerSwitchSystem.NextTurn(this);
             }
             _gameInfo.ColorToMove = _playerSwitchSystem.CurrentPlayer.Color;
-            if (isChess && _rules[RuleNames.Mate].IsSatisfied())
+            if (_rules[RuleNames.Mate].IsSatisfied())
                 _eventAggregator.GetEvent<Chess.Infrastructure.Events.MessageEvent>().Publish(new MessageInfo(0, "You lost! King in check-mate."));
         }
 
